6feedf6759fb149c9b9ce2391951c86c496b7e5c
Mohammed Alman <73015997+4lmxn@users.noreply.github.com>
2026-04-30 01:24:19 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,16 @@
+import gensim.downloader as api
+import matplotlib.pyplot as plt
+from sklearn.decomposi on import PCA
+wv = api.load("glove-wiki-gigaword-100")
+words = ["king","queen","man","woman","prince","princess"]
+vectors = [wv[w] for w in words]
+pca = PCA(n_components=2)
+result = pca.fit_transform(vectors)
+plt.figure(figsize=(8,6))
+for i, w in enumerate(words):
+plt.sca er(result[i,0], result[i,1])
+plt.text(result[i,0], result[i,1], w)
+plt. tle("Word Embeddings (PCA)")
+plt.show()
+print("\nSimilar words to 'king':")
+print(wv.most_similar("king", topn=5))
