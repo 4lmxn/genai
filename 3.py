5ec2d73c2bb270b45765514d0dac8cc52d27a0d5
Mohammed Alman <73015997+4lmxn@users.noreply.github.com>
2026-04-30 01:31:01 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,13 +1,17 @@
 from gensim.models import Word2Vec
+
 corpus = [
-"pa ent has fever",
-"doctor gives treatment",
-"medicine helps recovery",
-"virus causes infec on"
+    "patient has fever",
+    "doctor gives treatment",
+    "medicine helps recovery",
+    "virus causes infection"
 ]
+
 sentences = [s.split() for s in corpus]
 model = Word2Vec(sentences, vector_size=50, window=3, min_count=1)
+
 print("\nSimilar to 'treatment':")
 print(model.wv.most_similar("treatment"))
+
 print("\nSimilar to 'virus':")
 print(model.wv.most_similar("virus"))
