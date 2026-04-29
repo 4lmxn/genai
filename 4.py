2cb8e4bfde3a70cc123cada3f0e8a5dfc5a30e7c
Mohammed Alman <73015997+4lmxn@users.noreply.github.com>
2026-04-30 01:24:56 +0530

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,13 @@
+import gensim.downloader as api
+from transformers import pipeline
+wv = api.load("glove-wiki-gigaword-100")
+generator = pipeline("text-genera on", model="gpt2")
+def enrich(prompt, word):
+similar = wv.most_similar(word, topn=1)[0][0]
+return prompt.replace(word, similar)
+prompt = "Who is king"
+new_prompt = enrich(prompt, "king")
+print("\nOriginal Prompt:")
+print(generator(prompt, max_length=50)[0]['generated_text'])
+print("\nEnhanced Prompt:")
+print(generator(new_prompt, max_length=50)[0]['generated_text'])
